e69f7bc2718296132c2412d232fed1248bd69897
anonymous <anonymous@example.com>
2023-07-28 15:40:14 -0400

    spec of what to do with api

--- graffiti-p2p.js
+++ graffiti-p2p.js
@@ -5,8 +5,25 @@ import GraffitiContext from './src/context'
 import { randomHash } from "./src/util"
 
 // TODO:
-// Make sign only ask for a single authentication
-// context+posts returned by usePosts. posts.add(), posts.delete()??
+
+// const obj = gf.create(actor, value)
+// .set("x", "y")
+// .addContext(`actor:${actor}`)
+// .addContext(`tag:stuff`) // If you just have access to one context you shouldn't be able to see the others
+// .post() // sign and gossip over object channel and all context channels
+
+// obj.x = "y"
+
+// gf.modify(obj)
+// .set("x", "y")
+// .set(o=>o.x.y.z, "qwerty")
+// .removeContext("stuff")
+// .addContext("somethingElse")
+// .post()
+
+// gf.delete(obj) // deletes all properties and removes from all contexts, no post required
+
+// const { posts } = gf.useContext(`actor:${actor}`)
 
 // - store stuff in IDB
 // - make an actual demo
@@ -27,7 +44,7 @@ export default class Graffiti {
     return await this.actorClient.selectActor()
   }
 
-  async post(value, actor, container) {
+  async post(value, actor) {
     const object = this.wrapper.get(GraffitiObject, actor, await randomHash(), this.objectContainer)
     await object.set(value)
     return object.value
