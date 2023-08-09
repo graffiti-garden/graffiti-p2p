46fb35e873be31b27c783e47c571236f51180cb0
anonymous <anonymous@example.com>
2023-08-09 00:40:46 -0400

    no more https

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import mkcert from 'vite-plugin-mkcert'
 
 export default defineConfig({
-  plugins: [vue(), mkcert()],
+  plugins: [vue()],
   root: './demo',
-  server: {
-    https: true
-  }
 })
