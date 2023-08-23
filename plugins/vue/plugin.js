6e0f5d2f70c064c63fda0f79b95d4f844a8bc287
anonymous <anonymous@example.com>
2023-08-23 12:06:45 -0400

    Build for esmg

--- plugins/vue/plugin.js
+++ plugins/vue/plugin.js
@@ -1,105 +1,102 @@
 import Graffiti from '../../graffiti-p2p.js'
 import GraffitiPosts from './GraffitiPosts.vue'
 import GraffitiPostArray from '../../src/posts.js'
+import { reactive, isRef, watch, onScopeDispose, computed } from 'vue'
 
 const REFRESH_RATE = 100 // milliseconds
 
-export default function GraffitiPlugin(Vue) {
-  return {
-    install(app, options) {
-      const graffiti = new Graffiti({
-        ...options,
-        objectContainer: ()=> Vue.reactive({})
-      })
+export default function install(app, options) {
+  const graffiti = new Graffiti({
+    ...options,
+    objectContainer: ()=> reactive({})
+  })
 
-      // A composable that returns a collection of objects
-      graffiti.usePosts = contextPath=> {
-        const postMap = Vue.reactive({})
+  // A composable that returns a collection of objects
+  graffiti.usePosts = contextPath=> {
+    const postMap = reactive({})
 
-        // Run the loop in the background
-        let running = true
-        let controller
-        let timeoutID = null
-        let unwatch = ()=>{}
-        ;(async ()=> {
+    // Run the loop in the background
+    let running = true
+    let controller
+    let timeoutID = null
+    let unwatch = ()=>{}
+    ;(async ()=> {
 
-          while (running) {
-            controller = new AbortController();
-            const signal = controller.signal;
+      while (running) {
+        controller = new AbortController();
+        const signal = controller.signal;
 
-            unwatch =
-              Vue.isRef(contextPath)?
-                Vue.watch(contextPath, ()=> {
-                  // Clear the object map and restart the loop
-                  Object.keys(postMap).forEach(k=> delete postMap[k])
-                  controller.abort()
-                  unwatch()
-                  clearTimeout(timeoutID)
-                  timeoutID = null
-                }) : ()=>{}
+        unwatch =
+          isRef(contextPath)?
+            watch(contextPath, ()=> {
+              // Clear the object map and restart the loop
+              Object.keys(postMap).forEach(k=> delete postMap[k])
+              controller.abort()
+              unwatch()
+              clearTimeout(timeoutID)
+              timeoutID = null
+            }) : ()=>{}
 
-            // Unwrap more and stream changes into batches
-            const batch = {}
-            const postIterator = graffiti.posts(
-              Vue.isRef(contextPath)?
-              contextPath.value :
-              contextPath,
-              signal)
-            try {
-              for await (const postUpdate of postIterator) {
-                batch[postUpdate.hashURI] = postUpdate
+        // Unwrap more and stream changes into batches
+        const batch = {}
+        const postIterator = graffiti.posts(
+          isRef(contextPath)?
+          contextPath.value :
+          contextPath,
+          signal)
+        try {
+          for await (const postUpdate of postIterator) {
+            batch[postUpdate.hashURI] = postUpdate
 
-                // Flush the batch after timeout
-                if (!timeoutID) {
-                  timeoutID = setTimeout(()=> {
-                    for (const update of Object.values(batch)) {
-                      if (update.action == "add") {
-                        postMap[update.hashURI] = update.value
-                      } else {
-                        if (update.hashURI in postMap)
-                          delete postMap[update.hashURI]
-                      }
-                    }
-                    timeoutID = null
-                  }, REFRESH_RATE)
+            // Flush the batch after timeout
+            if (!timeoutID) {
+              timeoutID = setTimeout(()=> {
+                for (const update of Object.values(batch)) {
+                  if (update.action == "add") {
+                    postMap[update.hashURI] = update.value
+                  } else {
+                    if (update.hashURI in postMap)
+                      delete postMap[update.hashURI]
+                  }
                 }
-              }
-            } catch (e) {
-              if (e.code != DOMException.ABORT_ERR) {
-                throw e
-              }
+                timeoutID = null
+              }, REFRESH_RATE)
             }
           }
-        })()
-
-        Vue.onScopeDispose(()=> {
-          // Stop the loop
-          running = false
-          controller.abort()
-          unwatch()
-          clearTimeout(timeoutID)
-        })
-
-        // Strip IDs
-        return { posts: Vue.computed(()=> new GraffitiPostArray(
-          graffiti,
-          Vue.isRef(contextPath)? contextPath.value : contextPath,
-          null,
-          ...Object.values(postMap)
-        ))}
+        } catch (e) {
+          if (e.code != DOMException.ABORT_ERR) {
+            throw e
+          }
+        }
       }
+    })()
 
-      // Begin to define a global property that mirrors
-      // the vanilla spec but with some reactive props
-      const glob = app.config.globalProperties
-      Object.defineProperty(glob, "$graffiti", { value: graffiti })
-      Object.defineProperty(glob, "$gf",       { value: graffiti })
+    onScopeDispose(()=> {
+      // Stop the loop
+      running = false
+      controller.abort()
+      unwatch()
+      clearTimeout(timeoutID)
+    })
 
-      // Provide it globally to setup
-      app.provide('graffiti', graffiti)
-
-      // Add the component
-      app.component('GraffitiPosts', GraffitiPosts)
-    }
+    // Strip IDs
+    return { posts: computed(()=> new GraffitiPostArray(
+      graffiti,
+      isRef(contextPath)? contextPath.value : contextPath,
+      null,
+      ...Object.values(postMap)
+    ))}
   }
+
+  // Begin to define a global property that mirrors
+  // the vanilla spec but with some reactive props
+  const glob = app.config.globalProperties
+  Object.defineProperty(glob, "$graffiti", { value: graffiti })
+  Object.defineProperty(glob, "$gf",       { value: graffiti })
+
+  // Provide it globally to setup
+  app.provide('graffiti', graffiti)
+
+  // Add the component
+  app.component('GraffitiPosts', GraffitiPosts)
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  root: './demo',
+  build: {
+    lib: {
+      entry: {
+        vanilla: resolve(__dirname, 'graffiti-p2p.js'),
+        vue: resolve(__dirname, 'plugins/vue/plugin.js'),
+      },
+      fileName: (fmt, name)=> `${name}.${fmt}.js`
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
 })
