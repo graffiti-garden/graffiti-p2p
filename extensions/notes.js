d10684943b380db034da954e469569b6ed6f1e18
anonymous <anonymous@example.com>
2023-10-30 18:29:08 -0400

    audio file example in demo

---
+++ extensions/notes.js
@@ -0,0 +1,30 @@
+// Create object
+// set/change/delete parameter
+// Delete object
+
+// type: 'ObjectReference'
+// uuid: 'ddfkjk'
+
+noteReferenceValidate() {}
+
+noteLink() {
+  const uuid = crypto.randomUUID()
+  const source = {
+    type: 'NoteReference',
+    reference: uuid,
+    modifiers: []
+  }
+
+  const target = {
+    type: 'Note',
+    uuid: uuid,
+    published,
+    content,
+    image,
+    title
+  }
+}
+
+noteLinkCallback
+
+// edit the note
